4a94103b6233072938e85a1061e37924b97e3530
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2025-04-16 11:24:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -68,3 +68,7 @@ def create_pdf():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))  # Render sets this automatically
+    app.run(host='0.0.0.0', port=port)
