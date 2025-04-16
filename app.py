fac7e491a54686c84897275681a5f101e5e82015
Navneet Lamba <70816895+lnavneet331@users.noreply.github.com>
2025-04-16 11:05:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,5 @@ def create_pdf():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT not set
+    app.run(host='0.0.0.0', port=port)
